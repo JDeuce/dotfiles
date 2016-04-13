cfae9560d7a5b8c3e5fab79e6109a840f579be64
Josh Jaques <jjaques@gmail.com>
2016-04-13 15:01:48 -0500

    add history file for python command line

--- .pythonrc.py
+++ .pythonrc.py
@@ -1,4 +1,6 @@
 #.pythonrc.py
+
+import os
 try:
     import readline
 except ImportError:
@@ -6,3 +8,13 @@ except ImportError:
 else:
     import rlcompleter
     readline.parse_and_bind("tab: complete")
+
+    histfile = os.path.join(os.path.expanduser("~"), ".pyhist")
+    try:
+        readline.read_history_file(histfile)
+        readline.set_history_length(1000)
+    except IOError:
+        pass
+
+    import atexit
+    atexit.register(readline.write_history_file, histfile)
